33d0d40694e9cb38e63e5edf6c1b6d950fca5634
kouta1612 <kouta1612world69@gmail.com>
2021-12-14 09:51:41 +0900

    apollo connect to express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,26 @@
 import "reflect-metadata";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
 (async () => {
     const app = express()
-    app.get('/', (_, res) => res.send('hello'))
+    app.get('/', (_req, res) => res.send('hello'))
+
+    const apolloServer = new ApolloServer({
+        typeDefs: `
+            type Query {
+                hello: String!
+            }
+        `,
+        resolvers: {
+            Query: {
+                hello: () => "hello world"
+            }
+        }
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({ app })
+
     app.listen(4000, () => console.log('express server started.'))
 })()
 
