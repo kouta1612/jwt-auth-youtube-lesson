c8e346c0142d752b4a536dc98b63978b619a56d3
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2021-12-15 12:57:01 +0900

    create login function

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,13 @@
-import { hash } from "bcryptjs";
-import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import { compare, hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { Resolver, Query, Mutation, Arg, ObjectType, Field } from "type-graphql";
 import { User } from "./entity/User";
+
+@ObjectType()
+class LoginResponse {
+    @Field()
+    accessToken: string
+}
 
 @Resolver()
 export class UserResolver {
@@ -31,5 +38,28 @@ export class UserResolver {
             return false
         }
         return true
+    }
+
+    @Mutation(() => LoginResponse)
+    async login(
+        @Arg('email') email: string,
+        @Arg('password') password: string
+    ): Promise<LoginResponse> {
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            throw new Error("could not find user.");
+        }
+
+        const valid = await compare(password, user.password)
+
+        if (!valid) {
+            throw new Error("bad passowrd.");
+        }
+        
+        return {
+            accessToken: sign({userId: user.id}, 'vsdkjvsdnvkd', {
+                expiresIn: "15m"
+            })
+        }
     }
 }
