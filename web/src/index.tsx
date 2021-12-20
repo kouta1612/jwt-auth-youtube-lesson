3da92f27d8d67d68e5da4e9144de859afbdaebcc
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2021-12-20 13:00:55 +0900

    add graphql client

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks'
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+})
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
